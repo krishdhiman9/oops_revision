bdf73dc19f4f628a16bc2b8bcde20081e611fa86
krish dhiman <krishdhiman86@gmail.com>
2026-07-08 21:43:41 +0530

    practice question

---
+++ encapsulation/revision.java
@@ -0,0 +1,20 @@
+package encapsulation;
+class student{
+    private  int roll = 0 ;
+
+    public void setRoll(int r){
+        roll = r;
+    }
+    public int getRoll(){
+        return roll;
+    }
+}
+public class revision  {
+    static void main() {
+        student n = new student();
+        n.setRoll(5);
+        System.out.println(n.getRoll());
+
+
+    }
+}
