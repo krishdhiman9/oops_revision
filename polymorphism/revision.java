45e5fc1c922c0f969fbf5f173449e3ac9acae86f
krish dhiman <krishdhiman86@gmail.com>
2026-07-09 19:11:23 +0530

    revision

--- polymorphism/revision.java
+++ polymorphism/revision.java
@@ -1,21 +1,48 @@
 package polymorphism;
 //method overloading
-class calculator{
-    int sum(int a,int b){
-        return a+b;
+//class calculator{
+//    int sum(int a,int b){
+//        return a+b;
+//    }
+//    double sum(double a,int b,int c){
+//        return a+b+c;
+//    }
+//    int sum(int a,int b,int c,int d){
+//        return a+b+c+d;
+//    }
+//}
+//
+//public class revision {
+//    public static void main() {
+//        calculator c = new calculator();
+//        System.out.println(c.sum(2,3,4,5));
+//    }
+//}
+
+//methor overriding
+class Animal {
+    void sound(){
+        System.out.println("meaoww");
     }
-    double sum(double a,int b,int c){
-        return a+b+c;
+}
+class dog extends Animal{
+    //@Override
+    void sound(){
+        System.out.println("dog barking");
     }
-    int sum(int a,int b,int c,int d){
-        return a+b+c+d;
+}
+class cow extends dog{
+    //@Override
+    void sound(){
+        System.out.println("cow's sound");
     }
 }
 
-public class revision {
-    public static void main() {
-        calculator c = new calculator();
-        System.out.println(c.sum(2,3,4,5));
-    }
-}
+public class revision{
+public static void main() {
+    Animal a = new Animal();
+    //cow c = new cow();
+    a.sound();
+
+}}
 
