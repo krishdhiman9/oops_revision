546832e731fca2a4f34ff0127d500bf26a2cd243
krish dhiman <krishdhiman86@gmail.com>
2026-07-09 18:56:14 +0530

    revision

--- polymorphism/revision.java
+++ polymorphism/revision.java
@@ -1,9 +1,22 @@
 package polymorphism;
-
+//method overloading
+class calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+    double sum(double a,int b,int c){
+        return a+b+c;
+    }
+    int sum(int a,int b,int c,int d){
+        return a+b+c+d;
+    }
+}
 
 
 public class revision {
     public static void main() {
+        calculator c = new calculator();
+        System.out.println(c.sum(2,3,4,5));
 
     }
 }
