72ce8261f0dba2861ae0847d0b660645e320858b
krish dhiman <krishdhiman86@gmail.com>
2026-07-13 22:00:48 +0530

    basics

--- abstraction/basic.java
+++ abstraction/basic.java
@@ -1,7 +1,45 @@
 package abstraction;
+abstract class Animal {
+
+    // Abstract Method
+    abstract void sound();
+
+    // Normal Method
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child Class 1
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Child Class 2
+class Cat extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
 
 public class basic {
-    public static void main() {
 
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+        d.eat();
+
+        System.out.println();
+
+        Cat c = new Cat();
+        c.sound();
+        c.eat();
     }
 }
