cc35360eee9654ec7ee743e4e7b5f27144b1ade8
krish dhiman <krishdhiman86@gmail.com>
2026-07-19 22:28:45 +0530

    basics

--- abstraction/basic.java
+++ abstraction/basic.java
@@ -1,44 +1,61 @@
 package abstraction;
-abstract class Animal {
+//abstract class Animal {
+//
+//    // Abstract Method
+//    abstract void sound();
+//
+//    // Normal Method
+//    void eat() {
+//        System.out.println("Animal is eating");
+//    }
+//}
+//
+//// Child Class 1
+//class Dog extends Animal {
+//
+//    @Override
+//    void sound() {
+//        System.out.println("Dog barks...");
+//    }
+//}
+//
+//// Child Class 2
+//class Cat extends Animal {
+//
+//    @Override
+//    void sound() {
+//        System.out.println("Cat meows...");
+//    }
+//}
+//
+//public class basic {
+//
+//    public static void main(String[] args) {
+//
+//        Dog d = new Dog();
+//        d.sound();
+//        d.eat();
+//        System.out.println(" ");
+//
+//        Cat c = new Cat();
+//        c.sound();
+//        c.eat();
+//    }
+//}
 
-    // Abstract Method
+abstract class animal{
     abstract void sound();
-
-    // Normal Method
-    void eat() {
-        System.out.println("Animal is eating");
+    void eat(){
+        System.out.println("eating");
     }
 }
-
-// Child Class 1
-class Dog extends Animal {
-
-    @Override
-    void sound() {
-        System.out.println("Dog barks...");
+class dog extends animal{
+    void sound(){
+        System.out.println("barking");
     }
 }
-
-// Child Class 2
-class Cat extends Animal {
-
-    @Override
-    void sound() {
-        System.out.println("Cat meows...");
-    }
-}
-
-public class basic {
-
-    public static void main(String[] args) {
-
-        Dog d = new Dog();
-        d.sound();
-        d.eat();
-        System.out.println(" ");
-
-        Cat c = new Cat();
-        c.sound();
-        c.eat();
+class cat extends animal{
+    void sound(){
+        System.out.println();
     }
 }
