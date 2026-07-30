f009820b6c24afc33e77423cf4112240eac16b31
krish dhiman <krishdhiman86@gmail.com>
2026-07-30 21:35:59 +0530

    create class

---
+++ polymorphism/calc.java
@@ -0,0 +1,18 @@
+package polymorphism;
+class calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+    double sum(double a,int b,int c){
+        return a+b+c;
+    }
+    int sum(int a,int b,int c,int d){
+        return a+b+c+d;
+    }
+}
+
+public class calc {
+    static void main() {
+
+    }
+}
