21cfc728c5a6196b95ff7bca28ed88f06142105b
krish dhiman <krishdhiman86@gmail.com>
2026-07-31 21:37:43 +0530

    method overloading calc

--- polymorphism/calc.java
+++ polymorphism/calc.java
@@ -14,6 +14,6 @@ class calculator{
 public class calc {
     static void main() {
         calculator c = new calculator();
-        System.out.println(c.sum(12,3,4,5));
+        System.out.println(c.sum(12,3,44,5));
     }
 }
